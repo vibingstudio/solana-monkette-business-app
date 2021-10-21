e811236d646db503d8bdd27055e0814ba46dd377
eabz <eabz@polispay.org>
2021-10-20 19:15:47 -0500

    Trivial fixes

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -33,36 +33,38 @@ const Body = (): JSX.Element => {
             style={{ maxWidth: "1000px" }}
             className="mx-auto flex items-center justify-between w-full"
           >
-            <div className="grid grid-cols-1 md:grid-cols-2">
-              <div className="text-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+              <div className="text-center mx-auto">
                 <img
                   className="rounded-lg"
                   src={image}
-                  width="383px"
-                  height="383px"
                   alt="Solana Monkette Business"
                 />
               </div>
-              <div className="text-center rounded-lg bg-strong-pink m-4">
+              <div
+                className="mx-auto w-full text-center rounded-lg bg-strong-pink m-4"
+                style={{ maxWidth: "500px" }}
+              >
                 <h1 className="text-xl mt-10 text-white">
                   Solana Monkettes Business
                 </h1>
                 <p className="px-9 py-2 text-white">
-                  3333 generative female NFTs inspired by <b>@SolanaMBS</b> and{" "}
-                  <b>@MonkeDAO</b>
+                  3333 generative female NFTs inspired by{" "}
+                  <b>Solana Monkey Business</b> and the <b>Monke DAO</b>
                 </p>
                 <p className="px-9 py-2 text-white">
-                  Airdropped to SMB users that meet the requirements.
+                  Airdropped to <b>Solana Monkey Business</b> owners that meet
+                  the requirements at a random snapshot.
                 </p>
                 <p className="px-9 py-2 text-white">
-                  30% of mints donated to the @MonkeDAO
+                  30% of mints donated to the <b>Monke DAO</b>
                 </p>
-                <div className="text-center bg-pink text-xl border-white border-2 rounded-lg w-48 mx-auto mt-2">
+                <div className="text-center bg-pink text-xl border-white border-2 rounded-lg w-48 mx-auto my-2">
                   <button className="p-2 opacity-50 cursor-not-allowed">
                     <h1>Coming Soon</h1>
                   </button>
                 </div>
-                <div className="text-center bg-pink text-xl border-white border-2 rounded-lg w-48 mx-auto mt-2">
+                <div className="text-center bg-pink text-xl border-white border-2 rounded-lg w-48 mx-auto my-2">
                   <button className="p-2">
                     <h1>Marketplace</h1>
                   </button>
